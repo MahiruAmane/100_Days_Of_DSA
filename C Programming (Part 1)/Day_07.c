bb76cd062d9a48ff508f9df0381e8fc832f488e8
Stella Hoshino <piyush124790@gmail.com>
2026-02-07 15:44:51 +0530

    Day 7 Of C Programming
    Write a C Program To Print Fibonacci Series Using a Recursive Function.

---
+++ C Programming (Part 1)/Day_07.c
@@ -0,0 +1,36 @@
+// Write a C Program To Print Fibonacci Series Using a Recursive Function.
+
+#include <stdio.h>
+
+int fibonacci(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+int main()
+{
+    int n;
+
+    printf("Please Enter The Number Of Terms In Fibonacci Series : ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series : ");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", fibonacci(i));
+    }
+
+    return 0;
+}
